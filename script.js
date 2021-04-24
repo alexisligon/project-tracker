8eded53796bd8b3d04b130bc31c3b0748ac42ddd
Alexis Ligon <aligon17@gmail.com>
2021-04-24 13:53:17 -0400

    card

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function myTimer() {
     var date = moment().format("[Today:] MMM Do, YYYY  h:mm:ss a");
     $(".date").text(date);
   }
+
+// var projectButton = $("#project-button")
+// projectButton.on("click", function () {
+// })
+$('#myModal').on('shown.bs.modal', function () {
+    $('#exampleModal').trigger('focus')
+  })
