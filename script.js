10d924e94eea42a275080e93b60e35bc6c4cce32
Mark June <marcusjune95@gmail.com>
2021-04-24 12:45:17 -0400

    added timer script

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-script
+//for welcome message and timer
+var timer = setInterval(myTimer)
+
+function myTimer() {
+    var date = moment().format("[Today:] MMM Do, YYYY  h:mm:ss a");
+    $(".date").text(date);
+  }
